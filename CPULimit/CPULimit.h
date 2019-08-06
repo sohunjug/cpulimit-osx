a82b9055102f3958be45925dee7c4f6b2261d602
sohunjug <sohunjug@hotmail.com>
2019-08-06 14:23:27 +0800

    Initial Commit

---
+++ CPULimit/CPULimit.h
@@ -0,0 +1,19 @@
+//
+//  CPULimit.h
+//  CPULimit
+//
+//  Created by 胡文琦 on 2019/8/6.
+//  Copyright © 2019 sohunjug. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CPULimit.
+FOUNDATION_EXPORT double CPULimitVersionNumber;
+
+//! Project version string for CPULimit.
+FOUNDATION_EXPORT const unsigned char CPULimitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPULimit/PublicHeader.h>
+
+
